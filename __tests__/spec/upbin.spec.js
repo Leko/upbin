a1e998bd63671df80cae575544fe05b767d91da3
Leko <leko.noor@gmail.com>
2018-08-11 21:04:59 +0900

    Add tests for find and execute

--- __tests__/spec/upbin.spec.js
+++ __tests__/spec/upbin.spec.js
@@ -5,19 +5,42 @@ const { find, execute } = require("../../upbin");
 
 test("find should reject bin is not exists", async () => {
   try {
-    await find("xxx");
+    await find("xxx", { cwd: __dirname });
     assert.fail("Missing expected exception");
-  } catch (e) {} // eslint-disable-line no-empty
+  } catch (e) {
+    if (!/not found/.test(e.message)) {
+      assert.fail(e.message);
+    }
+  }
 });
 test("find should reject bin is not executable", async () => {
   try {
-    await find("not-executable");
+    await find("not-executable", { cwd: __dirname });
     assert.fail("Missing expected exception");
-  } catch (e) {} // eslint-disable-line no-empty
+  } catch (e) {
+    if (!/not executable/.test(e.message)) {
+      assert.fail(e.message);
+    }
+  }
 });
 test("find should resolve bin is executable", async () => {
-  const expected = path.resolve("fixtures", "executable");
-  assert.strictEqual(await find("executable"), expected);
+  const expected = path.resolve(
+    __dirname,
+    "..",
+    "node_modules",
+    ".bin",
+    "executable"
+  );
+  assert.strictEqual(await find("executable", { cwd: __dirname }), expected);
 });
 
-test("execute should through non-zero exit code", () => {});
+test("execute should through non-zero exit code", () => {
+  const bin = path.resolve(
+    __dirname,
+    "..",
+    "node_modules",
+    ".bin",
+    "non-zero-exit"
+  );
+  assert.strictEqual(execute(bin, []), 3);
+});
