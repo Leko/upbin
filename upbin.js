48ec844b3a25bb16134fee6e99345d4e90aa4dc5
Leko <leko.noor@gmail.com>
2018-08-11 19:58:42 +0900

    Add helpers

---
+++ upbin.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const { spawnSync } = require("child_process");
+const { promisify } = require("util");
+const findUp = require("find-up");
+
+const { X_OK } = fs.constants;
+const access = promisify(fs.access);
+
+const isExecutable = bin => access(bin, X_OK).then(() => true, () => false);
+
+const find = async binName => {
+  const bin = await findUp(path.join("node_modules", ".bin", binName));
+  if (!bin) {
+    throw new Error(`${binName} not found`);
+  }
+  if (!(await isExecutable(bin))) {
+    throw new Error(`${binName} is not executable`);
+  }
+
+  return bin;
+};
+
+const execute = (bin, args) => {
+  spawnSync(bin, args, { stdio: "inherit" });
+};
+
+module.exports = {
+  find,
+  execute
+};
