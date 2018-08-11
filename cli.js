bc363a1a8359161839c1ddf8b37eb6757dbe3860
Leko <leko.noor@gmail.com>
2018-08-11 19:58:50 +0900

    Add CLI enntrypoint

---
+++ cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const { find, execute } = require("./upbin");
+
+const cliArgs = process.argv.slice(2);
+if (cliArgs.length === 0) {
+  console.error(`Usage: upbin [binName] [args...]`); // eslint-disable-line no-console
+  process.exit(1);
+}
+
+find(cliArgs[0])
+  .then(bin => execute(bin, cliArgs.slice(1)))
+  .catch(error => {
+    console.error(`Error: ${error.message}`); // eslint-disable-line no-console
+    process.exit(1);
+  });
